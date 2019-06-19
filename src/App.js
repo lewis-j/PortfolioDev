29a0e4f726663545346018912168dacc7297fcdf
Lindsey <desmotion27@gmail.com>
2019-06-18 23:53:44 -0700

    parallax scroll working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component} from "react";
 import Nav from "./navbar.js";
 import Portfolio from "./portfolio.js";
+import Plx from 'react-plx';
 
 function importAll(r) {
   return r.keys().map(r);
@@ -25,13 +26,43 @@ const portfolioData = [
     
 ];
 
+const parallaxData = [
+  {
+    start: '.portfolio',
+    duration: 200,
+    properties: [
+      {
+        startValue: 1,
+        endValue: 0,
+        property: 'opacity',
+      },
+         {
+        startValue: 1,
+        endValue: 0,
+        property: 'opacity',
+      },
+    ],
+  },
+];
+
 class App extends Component{
     
   render(){
     return(
         
         <div>
-        <Nav />
+     
+        <Plx
+        className='MyAwesomeParallax'
+        onPlxEnd = { ()=>{console.log("plx ran")}}
+        parallaxData={ parallaxData }
+animateWhenNotInViewport = {true}
+      >
+        <img className="banner-img" src={images[0]}/>
+      </Plx>
+        <section className="b-container">
+        <h1 className="title">upcoming webdeveloper for hire</h1>
+        </section>
         <Portfolio data = { portfolioData } />
         </div>
     );
--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ export default class Portfolio extends Component{
     render() {
 
     return(
-    <Container> 
+    <Container  className="portfolio" > 
         <Row>
         {
         this.props.data.map((item, index) => <Col key = {index} md="4" sm="6" ><Card title = {item.title} imgSrc = {item.imgSrc} /> </Col>)
